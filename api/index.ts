cca30ebed0aa19a6be9c9a008da1a6443fb8e152
gablilli <167802854+gablilli@users.noreply.github.com>
2026-07-26 16:09:36 +0200

    chore: delete api directory

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import { createApp } from "../server/_core/app";
-
-const app = createApp();
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  return app(req as any, res as any);
-}
